1599a36815d586120ebef3ac7d45f95c946ff7c3
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-18 23:51:45 -0400

    Update Practice_Questions.sql

--- Chinook/Practice_Questions.sql
+++ Chinook/Practice_Questions.sql
@@ -1,27 +1,27 @@
-Show Customers (their full names, customer ID, and country) who are not in the US. (Hint: != or <> can be used to say "is not equal to").
+-- Show Customers (their full names, customer ID, and country) who are not in the US. (Hint: != or <> can be used to say "is not equal to").
 
-Show only the Customers from Brazil.
+-- Show only the Customers from Brazil.
 
-Find the Invoices of customers who are from Brazil. The resulting table should show the customer's full name, Invoice ID, Date of the invoice, and billing country.
+-- Find the Invoices of customers who are from Brazil. The resulting table should show the customer's full name, Invoice ID, Date of the invoice, and billing country.
 
-Show the Employees who are Sales Agents.
+-- Show the Employees who are Sales Agents.
 
-Find a unique/distinct list of billing countries from the Invoice table.
+-- Find a unique/distinct list of billing countries from the Invoice table.
 
-Provide a query that shows the invoices associated with each sales agent. The resulting table should include the Sales Agent's full name.
+-- Provide a query that shows the invoices associated with each sales agent. The resulting table should include the Sales Agent's full name.
 
-Show the Invoice Total, Customer name, Country, and Sales Agent name for all invoices and customers.
+-- Show the Invoice Total, Customer name, Country, and Sales Agent name for all invoices and customers.
 
-How many Invoices were there in 2009?
+-- How many Invoices were there in 2009?
 
-What are the total sales for 2009?
+-- What are the total sales for 2009?
 
-Write a query that includes the purchased track name with each invoice line item.
+-- Write a query that includes the purchased track name with each invoice line item.
 
-Write a query that includes the purchased track name AND artist name with each invoice line item.
+-- Write a query that includes the purchased track name AND artist name with each invoice line item.
 
-Provide a query that shows all the Tracks, and include the Album name, Media type, and Genre.
+-- Provide a query that shows all the Tracks, and include the Album name, Media type, and Genre.
 
-Show the total sales made by each sales agent.
+-- Show the total sales made by each sales agent.
 
-Which sales agent made the most in sales in 2009?
+-- Which sales agent made the most in sales in 2009?
